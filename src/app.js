b48673372f0474c2f2635535a178b714423e540b
Aryan Baranwal <aryan123web@gmail.com>
2021-04-05 14:22:21 +0545

    jwt,cookie,dotenv

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
+require('dotenv').config();
 const express = require("express");
 const path = require("path");
 const hbs = require('hbs');
 const app = express();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require('dotenv');
+const cookieParser = require("cookie-parser");
+const auth = require("./middleware/auth");
+
 require("./db/conn");
 const Register = require("./models/registers");
+const { json } = require("express");
+const { log } = require("console");
 
 const port = process.env.PORT || 3000;
 
@@ -12,6 +21,7 @@ const template_path = path.join(__dirname, "./templates/views" );
 const partial_path = path.join(__dirname, "./templates/partials" );
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(static_path));
@@ -19,8 +29,37 @@ app.set("view engine", "hbs");
 app.set("views", template_path);
 hbs.registerPartials(partial_path);
 
+// console.log(process.env.SECRET_KEY);
+
 app.get("/", (req,res) => {
     res.render("index")
+});
+
+app.get("/secret",auth, (req,res) => {
+    // console.log(`this is the cookie awesome ${req.cookies.jwt}`);
+    res.render("secret")
+});
+
+app.get("/logout",auth, async (req,res) => {
+   try {
+       console.log("req.user");
+      
+                        // for single logout
+    //    req.user.tokens = req.user.tokens.filter((currElement)=>{
+    //        return currElement.token !== req.token
+    //    });
+                            // logout from all devices
+                req.user.tokens = [];
+
+
+       res.clearCookie("jwt");
+       console.log("logout successfully");
+       
+       await req.user.save();
+       res.render("login");
+   } catch (error) {
+       res.status(500).send(error);
+   }
 });
 
 app.get("/register", (req,res) => {
@@ -46,16 +85,28 @@ app.post("/register", async (req,res) => {
             address : req.body.address,
             Password : password,
             ConfirmPassword : cpassword
-          });
+        });
+    console.log("the success part" + registerClient);
 
-        const registered = await registerClient.save();
-        res.status(201).render("login");
+    const token = await registerClient.generateAuthToken(); 
+    console.log("the token part" + token); 
+
+    res.cookie("jwt",token, {
+        expires:new Date(Date.now()+600000),
+        httpOnly:true
+    });
+    console.log(cookie); 
+
+    const registered = await registerClient.save();
+    console.log("the page part" + registered); 
+    res.status(201).render("login");
 
       }else {
           res.send("password arenot matching");
       }
-  }catch(e) {
-      res.status(400).send(e);
+    }catch(e) {
+         res.status(400).send(e);
+        console.log("the error part page");
   }
 });
 
@@ -67,10 +118,22 @@ app.post("/login", async (req,res) => {
            const useremail = await Register.findOne({email:email});
         // res.send(useremail.password);
         // console.log(useremail);
-        if(useremail.Password === password) {
+
+        const isMatch = await bcrypt.compare(password, useremail.Password)
+        
+    const token = await useremail.generateAuthToken(); 
+    console.log("the token part" + token); 
+
+    res.cookie("jwt",token, {
+        expires:new Date(Date.now()+600000),
+        httpOnly:true,
+        // secure:true
+    });
+    
+        if(isMatch) {
             res.status(201).render("index");
         } else {
-            res.send("Invalid login details");
+            res.send("Invalid pas details");
         }
 
     }catch(e) {
@@ -78,6 +141,24 @@ app.post("/login", async (req,res) => {
     }
 });
 
+// const bcrypt = require("bcryptjs");
+// const securePassword = async (password) => {
+//     const passwordHash = await bcrypt.hash(password,10);
+//     console.log(passwordHash);
+//     const passwordMatch = await bcrypt.compare("thapa@123",passwordHash);
+//     console.log(passwordMatch);
+// }
+// securePassword("thapa@123");
+
+// const createToken = async() => {
+//     const token = await jwt.sign({_id:"606956a3c07b27410865a22a"},"abcdwfqwertyuioplkjhgfdsazxcvbnmgfhgfdfdfgdghfjhghb",{
+//         expiresIn:"2 minutes"
+//     });
+//     console.log(token);
+//     const userVer = await jwt.verify(token,"abcdwfqwertyuioplkjhgfdsazxcvbnmgfhgfdfdfgdghfjhghb");
+//     console.log(userVer);
+// };
+// createToken();
 
 app.listen(port, () => {
     console.log(`connection is setup at ${port}`);
--- src/models/registers.js
+++ src/models/registers.js
@@ -1,5 +1,10 @@
+require('dotenv').config();
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const dotenv = require('dotenv');
+
 
 const clientSchema = new mongoose.Schema({
     FirstName : {
@@ -48,8 +53,36 @@ const clientSchema = new mongoose.Schema({
         type:String,
         required:true,
     },
-
+    tokens : [{
+        token:{
+            type:String,
+            required:true,
+        }
+    }]
 });
+clientSchema.methods.generateAuthToken = async function(){
+    try {
+        console.log(this._id);
+        const token = jwt.sign({_id:this._id.toString()},process.env.SECRET_KEY);
+        this.tokens = this .tokens.concat({token:token}) 
+        await this.save();
+        return token;
+    } catch (error){
+         res.send("the error part" + error);   
+         console.log("the error part" + error);
+    }
+};
+
+clientSchema.pre("save",async function(next) {
+    if(this.isModified("Password")){
+        // console.log(`the current password is ${this.Password}`);
+        this.Password = await bcrypt.hash(this.Password,10);
+        // console.log(`the current password is ${this.Password}`);
+        this.ConfirmPassword = await bcrypt.hash(this.Password,10);
+    }
+    next();
+});
+
 
 const Register = new mongoose.model('Register', clientSchema);
 
